e8cf677bc0c6ce92dfbea7da5dd34c22002cea11
Luis Dudamel <ladusa@gmail.com>
2022-05-03 10:50:22 +0200

    First dropdown html skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Dropdown from "./components/Dropdown/Dropdown";
 import Header from "./components/HeaderComponent/HeaderComponent";
 
 function App() {
   return (
     <>
       <Header />
+      <Dropdown />
     </>
   );
 }
