10b439a7afcce5a1eb6b0aefff2d8b62b12273b2
Luis Dudamel <ladusa@gmail.com>
2022-05-03 09:52:46 +0200

    Sonarcloud workflow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import DropdownComponent from "./components/DropdownComponent/DropdownComponent";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar homeText={"Home"} favText={"Favorites"} />
+      <DropdownComponent />
     </>
   );
 }
