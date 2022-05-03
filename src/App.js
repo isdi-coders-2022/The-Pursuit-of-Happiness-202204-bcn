9b557fee4f383c0b53f4f795b458d5968ea56def
Ignasi Reixach <ignasireixach@gmail.com>
2022-05-03 13:31:34 +0200

    Star Rating Component Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header/Header";
+import StarRating from "./components/StarRating/StarRating";
 
 function App() {
   return (
     <>
       <Header />
-      <Header />
+      <StarRating />
     </>
   );
 }
---
+++ src/components/StarRating/StarRating.js
@@ -0,0 +1,21 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const StyledStarRatingComponent = styled.div`
+  color: rgba(226, 182, 22, 1);
+  .star {
+    font-size: 22px;
+  }
+`;
+
+const StarRating = ({ rating }) => {
+  return (
+    <StyledStarRatingComponent>
+      <FontAwesomeIcon data-testid className="star" icon={faStar} />
+      <p>{rating}</p>
+    </StyledStarRatingComponent>
+  );
+};
+
+export default StarRating;
