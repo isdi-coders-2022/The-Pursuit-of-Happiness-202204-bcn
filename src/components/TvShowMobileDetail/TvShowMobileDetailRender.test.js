7914df01ec720588475af60335f3d1d3709a0fb7
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-05-03 10:03:49 +0200

    Fix test render TvShowMobileDetail

--- src/components/TvShowMobileDetail/TvShowMobileDetailRender.test.js
+++ src/components/TvShowMobileDetail/TvShowMobileDetailRender.test.js
@@ -1,20 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import TvShowMobileDetail from "./TvShowMobileDetail";
 
-test("Render component TvShowMobileDetailRender", () => {
-  render(
-    <TvShowMobileDetail
-      name="Stars Wars"
-      posterURL="url"
-      rating="10.0"
-      year="2020"
-      genre="Acción"
-    />
-  );
+describe("Given a component TvShowMobileDetail", () => {
+  describe("When it receives name=Stars Wars posterURL=url rating=10.0 year=2020 genre=Acción", () => {
+    test("Render component TvShowMobileDetailRender", () => {
+      render(
+        <TvShowMobileDetail
+          name="Stars Wars"
+          year="2020"
+          rating="10.0"
+          genre="Acción"
+          posterURL="url"
+        />
+      );
 
-  const linkElement = screen.getByText("Stars Wars");
-  expect(linkElement).toBeInTheDocument();
-  // findAllByText;
-  // expect(linkElement).toHaveTextContent("Stars War");
-  // expect(screen.getByText("Stars War")).toBeInTheDocument();
+      const linkElement = screen.getByText("Stars Wars");
+      expect(linkElement).toBeInTheDocument();
+      // findAllByText;
+      // expect(linkElement).toHaveTextContent("Stars War");
+      // expect(screen.getByText("Stars War")).toBeInTheDocument();
+    });
+  });
 });
