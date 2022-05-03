e7a9187b329f7e71bb7ebdfb6414708821153e00
Ignasi Reixach <ignasireixach@gmail.com>
2022-05-03 13:33:34 +0200

    Fix app.js conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from "./components/Header/Header";
-import StarRating from "./components/StarRating/StarRating";
 
 function App() {
   return (
     <>
       <Header />
-      <StarRating />
     </>
   );
 }
