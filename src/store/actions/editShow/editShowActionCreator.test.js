2e3a25c9be4316b1edbb321af24f4394e1aab1f1
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-05-04 17:38:54 +0200

    Fix package.lock

--- src/store/actions/editShow/editShowActionCreator.test.js
+++
@@ -1,5 +0,0 @@
-describe("Given editShowActionCreator function", () => {
-  describe("When it receives the string 'Soprano'", () => {
-    test("Then it return an object with a propertry 'shows' with the value 'Soprano", () => {});
-  });
-});
