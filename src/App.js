fb80cd580039209a2bb265496f5ae9d0482aa456
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-05-04 12:53:56 +0200

    Add awasync await for retrieve shows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import Header from "./components/Header/Header";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+
+const StyledAppComponent = styled.div`
+  color: black;
+  display: flex;
+  .App {
+    margin: auto;
+  }
+  h4,
+  p {
+    font-size: 22px;
+    color: white;
+  }
+`;
+
+let index = 0;
+const getKey = (name) => {
+  let key = name + ++index;
+  return key;
+};
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const loadPost = async () => {
+      setLoading(true);
+      const response = await axios.get("https://api.tvmaze.com/shows");
+      setPosts(response.data);
+      setLoading(false);
+    };
+    loadPost();
+  }, []);
+
   return (
-    <>
-      <Header />
-    </>
+    <StyledAppComponent key={getKey("style")}>
+      <div className="App">
+        {loading ? (
+          <h4>Loading...</h4>
+        ) : (
+          posts.map((item) => <h4 key={getKey("h4")}>{item.name}</h4>)
+        )}
+      </div>
+    </StyledAppComponent>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import { setupWorker, rest } from "msw";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const worker = setupWorker(
+//   rest.get("https://api.tvmaze.com/shows", (req, res, ctx) => {
+//     return res(ctx.json({ name: "John", url: "siteURL" }));
+//   })
+// );
+// worker.start();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
