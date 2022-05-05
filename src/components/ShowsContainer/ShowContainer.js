d4c7a9662c64f3870b441892494d8b7f76d94c8d
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-05-06 01:56:14 +0200

    Fix console errors

--- src/components/ShowsContainer/ShowContainer.js
+++ src/components/ShowsContainer/ShowContainer.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import shortid from "shortid";
 import TvShowMobile from "../TvShowMobile/TvShowMobile";
 
 const StyledShowsComponent = styled.div`
@@ -7,22 +7,25 @@ const StyledShowsComponent = styled.div`
   margin: 0;
   color: white;
 `;
+// const generateKey = (pre) => {
+//   return `${pre}_${new Date().getTime().toLocaleString()}`;
+// };
 
 const ShowContainer = ({ state, pageKey }) => {
   return (
     <StyledShowsComponent>
       {state.map((show) => (
-        <>
+        <div key={shortid.generate()}>
           <TvShowMobile
             showId={show.id}
-            key={pageKey + show.id}
+            key={shortid.generate()}
             name={show.name}
             posterURL={show.image.medium}
             year={show.premiered}
             genre={show.genres[0]}
             rating={show.rating.average}
           ></TvShowMobile>
-        </>
+        </div>
       ))}
     </StyledShowsComponent>
   );
